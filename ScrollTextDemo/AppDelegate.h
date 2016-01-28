c16668ebc0f77defd5dfad3c837e7fc7d9335ea7
Cloudox <cloudox@foxmail.com>
2016-01-28 17:21:20 +0800

    Initial Commit

---
+++ ScrollTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollTextDemo
+//
+//  Created by csdc-iMac on 16/1/28.
+//  Copyright (c) 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollTextDemo
+//
+//  Created by csdc-iMac on 16/1/28.
+//  Copyright (c) 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
