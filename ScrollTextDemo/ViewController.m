30349f8e9aa83fc567d1d4b8a829f8158c244d3a
Cloudox <cloudox@foxmail.com>
2016-01-28 17:24:03 +0800

    完成滚动效果

--- ScrollTextDemo/ViewController.m
+++ ScrollTextDemo/ViewController.m
@@ -16,7 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 200, 20)];
+    label.text = @"cloudox";
+    [self.view addSubview:label];
+    
+    CGRect frame = label.frame;
+    frame.origin.x = 0;
+    label.frame = frame;
+    
+    
+    [UIView beginAnimations:@"testAnimation" context:NULL];
+    [UIView setAnimationDuration:5.0f];
+    [UIView setAnimationCurve:UIViewAnimationCurveLinear];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationRepeatAutoreverses:NO];
+    [UIView setAnimationRepeatCount:999999];
+    
+    
+    frame = label.frame;
+    frame.origin.x = 200;
+    label.frame = frame;
+    [UIView commitAnimations];
 }
 
 - (void)didReceiveMemoryWarning {
